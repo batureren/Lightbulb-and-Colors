6e98477d2ab06bcfc4d314e1811601b3551e0619
Batur Eren <baturerenbey@gmail.com>
2023-01-15 23:49:27 +0300

    Lightbulb animation

---
+++ random.js
@@ -0,0 +1,43 @@
+//!Preload images on boot
+var images = [
+    "./img/light-on.png",
+    "./img/light-on-66.png",
+    "./img/light-on-33.png",
+    "./img/light-on-100.png"
+  ];
+  
+  // loop through the images array
+  for (var i = 0; i < images.length; i++) {
+      // create a new image element
+      var img = new Image();
+      img.src = images[i];
+  
+      // add the image element to the document
+      document.body.appendChild(img);
+  }
+
+//! Lightbulb area animation
+let area = document.querySelector("area")
+let lightbulb = document.getElementById("lightbulb")
+let bright = 0
+
+area.addEventListener("click", function() {
+    lighton();
+  });
+
+  
+  function lighton(){
+    if(bright === 0){
+        document.body.style.backgroundColor = "black";
+        lightbulb.src="./img/light-on-100.png";
+        bright++;
+        $("#lightbulb").addClass("lightbulb-animation")
+    }else{
+        document.body.style.backgroundColor = "black";
+        lightbulb.src="./img/light-off.png";
+        bright--;
+        $("#lightbulb").removeClass("lightbulb-animation")
+    }
+  }
+
+  //!
